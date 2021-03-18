298e67b1b06a0236ca6551bade3f899793b19e08
Kelvin Davis <273degreeskelvin@gmail.com>
2021-03-18 23:59:19 +1100

    Mouse up and down observable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { Observable, interval } from "rxjs";
+import { Observable, interval, fromEvent } from "rxjs";
 import {} from 'rxjs/operators';
+
+const mouseDownObs = fromEvent(document, 'mousedown');
+const mouseUpObs = fromEvent(document, 'mouseup');
+
+mouseDownObs.subscribe((e) => console.log("there we go!"));
+mouseUpObs.subscribe((e) => console.log("DROP THE MOUSE!"));
 
 console.log("hello");
 

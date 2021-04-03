d58ae672dff742b0f3523e0c72de314a143a7ab7
Kelvin Davis <273degreeskelvin@gmail.com>
2021-04-04 02:37:21 +1100

    Initialise internal and external arrays.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,65 @@
-import { Observable, interval, fromEvent } from "rxjs";
-import {} from 'rxjs/operators';
+import { Observable, interval, fromEvent, animationFrameScheduler } from "rxjs";
+import { mergeMap, takeUntil } from 'rxjs/operators';
 
-const mouseDownObs = fromEvent(document, 'mousedown');
-const mouseUpObs = fromEvent(document, 'mouseup');
+const SIZE = 28;
+const PIXEL_SIZE = 20;
+const GAP_SIZE = 2;
 
-mouseDownObs.subscribe((e) => console.log("there we go!"));
-mouseUpObs.subscribe((e) => console.log("DROP THE MOUSE!"));
+type Table = Array<Array<number>>;
 
-console.log("hello");
+function initTable(size:number): Table {
+    // creates a nxn array
+    const arr: Array<Array<number>> = new Array(size);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(size);
+        for (let j = 0; j < arr[i].length; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    return arr;
+}
 
-const obs: Observable<number> = interval(1000);
-obs.subscribe(console.log);
+function draw_pixel(
+    ctx: CanvasRenderingContext2D, table: Table, i: number, j: number
+) {
+    let x = GAP_SIZE + (PIXEL_SIZE + GAP_SIZE) * i;
+    let y = GAP_SIZE + (PIXEL_SIZE + GAP_SIZE) * j;
+    ctx.clearRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+    ctx.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+    ctx.fillStyle = table[i][j] === 1 ? '#FFF' : '#000';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// create canvas
+var svg = document.createElement('canvas');
+svg.setAttribute('width', '' + (GAP_SIZE + (PIXEL_SIZE + GAP_SIZE) * SIZE) + '');
+svg.setAttribute('height', '' + (GAP_SIZE + (PIXEL_SIZE + GAP_SIZE) * SIZE) + '');
+document.getElementById('canvas')!.appendChild(svg);
+var ctx = svg.getContext('2d')!;
+
+// create internal and external pixel array
+const arr = initTable(SIZE);
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        draw_pixel(ctx, arr, i, j);
+    }    
+}
+
+// create the Observable streams
+const mouseDownObs$ = fromEvent(svg, 'mousedown');
+const mouseUpObs$ = fromEvent(svg, 'mouseup');
+const mouseMoveObs$ = fromEvent(svg, 'mousemove');
+const obs$: Observable<number> = interval(1000);
+const frames$: Observable<number> = interval(0, animationFrameScheduler);
+
+// create a chain: mousedown -> mousemove -> mouseup
+mouseDownObs$.subscribe((_) => console.log("there we go!"));
+mouseDownObs$.pipe(
+    mergeMap((_) => mouseMoveObs$.pipe(takeUntil(mouseUpObs$)))
+).subscribe(console.log);
+mouseUpObs$.subscribe((_) => console.log("DROP THE MOUSE!"));
+
+// time dependent streams
+//obs.pipe(takeUntil(mouseUpObs)).subscribe(console.log);
+//frames$.pipe(takeUntil(mouseUpObs)).subscribe(console.log);
